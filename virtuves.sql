a6c3ce0197bb61e6a3855b506358ab9918a75b1f
shvinius <shvinius@inbox.lt>
2022-07-02 16:19:16 +0300

    Prideta virtuve ir alergijos

---
+++ virtuves.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `virtuves/salys` (
+  `id` TINYINT NOT NULL,
+  `pavadinimas` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `virtuves/salys`(`id`, `pavadinimas`) VALUES (1,'Vidurio Rytų'), (2,'Lebanietiška'), (3, 'Universali'), (4, 'Azijiečių'), (5, 'Morokietiška'), (6, 'Lietuviška')
---
+++ visos_alergijos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `visos_alergijos` (
+  `id` TINYINT NOT NULL,
+  `pavadinimas` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `visos_alergijos`(`id`, `pavadinimas`) VALUES (1,'glitimas'), (2, 'vėžiagyviai'), (3, 'kiaušiniai'), (4, 'žuvys'), (5, 'žemės riešutai'), (6, 'sojų pupelės'), (7, 'pienas'), (8, 'riešutai'), (9, 'salierai'), (10, 'garstyčios'), (11, 'sezamo sėklos'), (12, 'sieros dioksidas'), (13, 'lubinai'), (14,'moliuskai')
