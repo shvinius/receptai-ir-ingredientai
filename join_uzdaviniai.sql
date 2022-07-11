faf80c9135759d7db8eee48009625317b168be30
shvinius <shvinius@inbox.lt>
2022-07-11 09:14:50 +0300

    neisejo isspresti 5 ir 6 punkto

--- join_uzdaviniai.sql
+++ join_uzdaviniai.sql
@@ -47,5 +47,26 @@ WHERE
     r.id = 4    
 
 --5. Grąžinkite visas alergijas receptų, kurių kaina yra mažiau nei 7.20.    
+SELECT
+    *
+FROM
+    `receptas` r
+JOIN 
+    sujungimas_receptai_alergijos s ON r.id = s.receptas_id
+JOIN
+    visos_alergijos a ON a.id = s.visos_alergijos_id
+WHERE 
+    kaina < 7.20
 
+--6. Grąžinkite kokie ingredientai yra naudojami pirmame, antrame ir trečiame recepte, tačiau pasikartojančių ingredientų neišveskite antrą kartą (tam naudojamas SELECT DISTINCT)
+SELECT
+    DISTINCT
+FROM
+    `receptas` r
+JOIN    
+	sujungimas_receptai_ingredientai s ON r.id = s.receptas_id
+JOIN
+	ingredientas i ON i.id = s.ingredientas_id
+WHERE
+    r.id = 1, r.id = 2, r.id = 3,
 
